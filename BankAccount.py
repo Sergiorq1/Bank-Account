ecc4c97891063e183684c0bab89931f79104af5a
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2020-11-09 20:31:33 -0800

    make class and init attributes

---
+++ BankAccount.py
@@ -0,0 +1,6 @@
+class BankAccount:
+    def __init__(self, full_name, account_number, routing_number, balance):
+        self.full_name = full_name
+        self.account_number = account_number
+        self.routing_number = 192840982
+        self.balance = float(0)
